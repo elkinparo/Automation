ddf06fa396eb1210d5058fa2a6418e6c5d3b7f16
Elkin Dario Palacios Rodriguez <epalacios@sistecredito.com>
2022-12-05 18:59:15 -0500

    02/12/2022 v3

--- WEB/Reto1/src/main/java/com/reto/userinterfaces/Purchase.java
+++ WEB/Reto1/src/main/java/com/reto/userinterfaces/Purchase.java
@@ -3,16 +3,17 @@ package com.reto.userinterfaces;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-public class Purchase
-{
+public class Purchase {
     public static final Target ASK = Target.the("ASK")
             .located(By.xpath("//*[text()='Aceite de Girasol']//following::button"));
     public static final Target CONFIRMORDER = Target.the("CONFIRMORDER")
             .located(By.xpath("//button[@id='order-confirm']"));
     public static final Target CONFIRMATIONMESSAGE = Target.the("CONFIRMATIONMESSAGE")
-            .located(By.xpath("//*[contains(text(),'Tu pedido ha sido confirmado, te mantendremos informado ante nuevas novedades')]"));
+            .located(By.xpath("//*[contains(text(),'Tu pedido ha sido confirmado, " +
+                    "te mantendremos informado ante nuevas novedades')]"));
     public static final Target CLOSE = Target.the("CLOSE")
-            .located(By.xpath("//*[contains(text(),'Tu pedido ha sido confirmado, te mantendremos informado ante nuevas novedades')]//preceding::span[1]"));
+            .located(By.xpath("//*[contains(text(),'Tu pedido ha sido confirmado, " +
+                    "te mantendremos informado ante nuevas novedades')]//preceding::span[1]"));
     public static final Target MYORDERS = Target.the("MYORDERS")
             .located(By.xpath("//*[text()='Mis pedidos']"));
     public static final Target ORDERS = Target.the("ORDERS")
@@ -22,14 +23,14 @@ public class Purchase
     public static final Target CUPONCODE = Target.the("CUPONCODE")
             .located(By.xpath("//*[@class='coupon-code']"));
     public static final Target CUPONCLOSE = Target.the("CUPONCLOSE")
-            .located(By.xpath("//*[contains(text(),'Copia este código y úsalo para disfrutar de un descuento por única vez')]//preceding::span[2]"));
+            .located(By.xpath("//*[contains(text(),'Copia este código y úsalo " +
+                    "para disfrutar de un descuento por única vez')]//preceding::span[2]"));
 
     public static final Target CUPONINPUT = Target.the("CUPONINPUT")
             .located(By.xpath("//input[@id='coupon']"));
 
     public static final Target BEGINING = Target.the("BEGINING")
             .located(By.xpath("//*[contains(text(),'Inicio')]"));
-
 
 
 }
